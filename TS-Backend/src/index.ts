d20786269339a6ddc75afffe40bacc590fe5a371
Leonardo Osorio <leonardoosorio00@gmail.com>
2023-06-30 18:07:35 -0400

    Adding Packages

---
+++ TS-Backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import axios from 'axios'
+
+const app = express()
+const port = 4000
+
+app.get('/', (_req, res) => {
+  res.send('Hello World! from express')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
